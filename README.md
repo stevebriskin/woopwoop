129a250fea3e6a04a51ce775f2bcc687f02f465c
Steve Briskin <steve@viam.com>
2024-06-28 10:21:40 -0400

    updates to query param

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-Cloud function for the Woot Woot bot.
+# Cloud function for the Woot Woot bot.
+It is built with the viam micrordk on an esp32 w-rover with leds on pin 12 and a buzzer on pin 14.
 
-app.viam.com Config
+## app.viam.com Config
 ```json
 {
   "components": [
@@ -11,7 +12,8 @@ app.viam.com Config
       "model": "esp32",
       "attributes": {
         "pins": [
-          12
+          12,
+          14
         ]
       }
     }
@@ -19,7 +21,24 @@ app.viam.com Config
 }
 ```
 
-Message structure
+## Messages
+Two types of messages are supported -- GCP incident alert JSON in the body or query parameters. This will change in the future!
+
+### Query Parameter Example
+Note: The URL suffix and secret are stored as env variables
+
+The fuction supports 4 parameters:
+- secret - secret to authenticate this call
+- woop - which woop to activate (see code for uri naming assumption)
+- buzzer - control the buzzer on pin 14. Values: on|off
+- strobe - control the led strobe on pin 12. Values: on|off
+
+`curl -X GET 'https://us-central1-some-project.cloudfunctions.net/woop?woop=1\&secret=xyz&strobe=off&buzzer=on'`
+
+
+### GCP Alert Example
+
+#### Message structure
 ```json
 {
     "incident": {
@@ -29,7 +48,7 @@ Message structure
 }
 ```
 
-Example Usage (without auth)
+#### Example Usage (without auth)
 ```bash
 curl -m 130 -X POST https://gcp-project-123.cloudfunctions.net/woopwoop  -H "Content-Type: application/json" -d '{"incident": { "summary": "test", "state": "CLOSED"}}'
 ```
--- function.go
+++ function.go
@@ -18,37 +18,6 @@ import (
 	"go.viam.com/utils/rpc"
 )
 
-/*
-type GCPAlertIncident struct {
-	Incident_id            string `json:"incident_id"`            //string, generated ID for this incident.
-	Scoping_project_id     string `json:"scoping_project_id"`     //string, the project ID that hosts the metrics scope.
-	Scoping_project_number int    `json:"scoping_project_number"` //number, the project number of the scoping project.
-	Url                    string `json:"url"`                    //string, Google Cloud console URL for this incident.
-	Started_at             uint64 `json:"started_at"`             //number, time (in Unix epoch seconds) when the incident was opened.
-	Ended_at               uint64 `json:"ended_at"`               //number, time (in Unix epoch seconds) when the incident was closed. Populated only when state is closed.
-	State                  string `json:"state"`                  //string, state of the incident: open or closed. If open, then ended_at is null.
-	Summary                string `json:"summary"`                //string, generated textual summary of the incident.
-	Apigee_url             string `json:"apigee_url"`             //string, Apigee URL for this incident, only for Apigee resource types Environment and Proxy*.
-	Observed_value         string `json:"observed_value"`         //string, observed value that triggered/resolved the alert, may be empty if the condition is expired.
-
-	Resource map[string]any `json:"resource"`
-	Metric   map[string]any `json:"metric"`
-
-	Policy_name        string         `json:"policy_name"`        // string, display name for the alerting policy.
-	Policy_user_labels map[string]any `json:"policy_user_labels"` //"policy_user_labels": object, key-value pairs for any user labels attached to the policy.
-	Documentation      map[string]any `json:"documentation"`      //: object, an embedded structure of the form Documentation.
-	Condition          map[string]any `json:"condition"`          //: object, an embedded structure of the form Condition.
-	ConditionName      string         `json:"condition_name"`     //: string, display name of the condition, same value as condition.displayName.
-	Severity           string         `json:"severity"`           //: string, severity level of incidents. If this field isn't defined during alerting policy creation, then Cloud Monitoring sets the alerting policy severity to No Severity.
-	ThresholdValue     string         `json:"threshold_value"`    //: string, the threshold value of this condition, may be empty if the condition isn't a threshold condition.
-}
-
-type GCPalert struct {
-	Version  string           `json:"version"`
-	Incident GCPAlertIncident `json:"incident"`
-}
-*/
-
 func makeLogger(name string) golog.Logger {
 	cfg := zap.Config{
 		Level:    zap.NewAtomicLevelAt(zap.InfoLevel),
@@ -118,7 +87,18 @@ func init() {
 	functions.HTTP("woop", woop)
 }
 
+// helloHTTP is an HTTP Cloud Function with a request parameter.
 func woop(w http.ResponseWriter, r *http.Request) {
+	if len(r.URL.Query()) == 0 {
+		doGCPAlert(w, r)
+	} else {
+		doBasicQueryParam(w, r)
+	}
+}
+
+// TODO: original code, needs to be generalized and combined with the query param one. Mostly here for history.
+// This is meant for a GCP alert webhook
+func doGCPAlert(w http.ResponseWriter, r *http.Request) {
 	api_key := os.Getenv("api_key")
 	api_key_id := os.Getenv("api_key_id")
 	logger := makeLogger("client")
@@ -133,20 +113,13 @@ func woop(w http.ResponseWriter, r *http.Request) {
 	incident := alert["incident"].(map[string]any)
 	summary := incident["summary"].(string)
 	state := incident["state"].(string)
-	/*
-		var alert GCPalert
-		if err := json.NewDecoder(r.Body).Decode(&alert); err != nil {
-			logger.Error("failed to decode body into struct.", err)
-			return
-		}
-		logger.Debug(alert)
-		logger.Info("Recieved an alert with summary: ", alert.Incident.Summary)
-	*/
 
 	logger.Info("Recieved an alert with summary: ", summary)
 
 	fmt.Fprint(w, "connecting")
 	ctx := context.Background()
+
+	// TODO: don't hardcode
 	robot, err := RobotClient(ctx, "woop-woop-main.6xs7zv3bxz.viam.cloud", api_key, api_key_id, logger, 5)
 	if err != nil {
 		logger.Error("Failed to connect to robot.", err)
@@ -175,4 +148,81 @@ func woop(w http.ResponseWriter, r *http.Request) {
 		logger.Error("Couldn't set pin value", err)
 		return
 	}
+}
+
+// expect strobe, buzzer, and woop number as query parameters.
+// example: `curl -X GET 'https://us-central1-.cloudfunctions.net/woop?woop=3&secret=xyz&strobe=off&buzzer=on'`
+func doBasicQueryParam(w http.ResponseWriter, r *http.Request) {
+	api_key := os.Getenv("api_key")
+	api_key_id := os.Getenv("api_key_id")
+	uri_suffix := os.Getenv("uri_suffix")
+	secret := os.Getenv("secret")
+	logger := makeLogger("client")
+
+	// not good, but good enough for current usage
+	passed_secret := r.URL.Query().Get("secret")
+
+	if secret != passed_secret {
+		logger.Error("Wrong secret")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// don't want to commit full URIs...the suffix with the location is an env variable
+	woop_num := r.URL.Query().Get("woop")
+	robot_uri_full := fmt.Sprintf("woopwoop%s-main.%s", woop_num, uri_suffix)
+
+	logger.Info("Connecting to ", robot_uri_full)
+	ctx := context.Background()
+	robot, err := RobotClient(ctx, robot_uri_full, api_key, api_key_id, logger, 5)
+	if err != nil {
+		logger.Error("Failed to connect to robot.", err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	defer robot.Close(ctx)
+
+	esp, err := board.FromRobot(robot, "board")
+	if err != nil {
+		logger.Error("No board found.", err)
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+	defer esp.Close(ctx)
+
+	strobePin, err := esp.GPIOPinByName("12")
+	if err != nil {
+		logger.Error("Strobe gpio pin 12 not found.", err)
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+
+	buzzerPin, err := esp.GPIOPinByName("14")
+	if err != nil {
+		logger.Error("Buzzer gpio pin 14 not found.", err)
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+
+	strobeValue := strings.EqualFold(r.URL.Query().Get("strobe"), "on")
+	buzzerValue := strings.EqualFold(r.URL.Query().Get("buzzer"), "on")
+	logger.Infof("Request: Strobe: %v, Buzzer: %v", strobeValue, buzzerValue)
+
+	logger.Info("Setting strobe pin to ", strobeValue)
+	err = strobePin.Set(ctx, strobeValue, nil)
+	if err != nil {
+		logger.Error("Couldn't set pin value", err)
+		w.WriteHeader(http.StatusRequestTimeout)
+		return
+	}
+
+	logger.Info("Setting buzzer pin to ", buzzerValue)
+	err = buzzerPin.Set(ctx, buzzerValue, nil)
+	if err != nil {
+		logger.Error("Couldn't set pin value", err)
+		w.WriteHeader(http.StatusRequestTimeout)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
